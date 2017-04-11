2e85e0cd7d248f5c9785a14d8821151ec919bb9e
Junior Burleon <jburleon@excilys.com>
2017-04-11 14:47:40 +0200

    Config for Jenkins and Docker

---
+++ dockerfile/mysqldock/2-PRIVILEGES.sql
@@ -0,0 +1,9 @@
+ #-----------------------------------
+ #USER RIGHTS MANAGEMENT
+ #-----------------------------------
+ CREATE USER 'travis'@'172.18.0.1' IDENTIFIED BY 'apql1161713';
+
+ GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'travis'@'172.18.0.1' WITH GRANT OPTION;
+
+
+ FLUSH PRIVILEGES;
