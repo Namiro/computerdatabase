38d37d0977d502bfb0da5b0b11407754e94f3322
Junior Burleon <jburleon@excilys.com>
2017-04-19 11:22:42 +0200

    FIX (Bad) For connection privilege

--- dockerfile/prodmysqldock/2-PRIVILEGES.sql
+++ dockerfile/prodmysqldock/2-PRIVILEGES.sql
@@ -1,9 +1,9 @@
  #-----------------------------------
  #USER RIGHTS MANAGEMENT
  #-----------------------------------
- CREATE USER 'travis'@'35.177.232.151' IDENTIFIED BY 'apql1161713';
+ CREATE USER 'travis'@'%' IDENTIFIED BY 'apql1161713';
 
- GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'travis'@'35.177.232.151' WITH GRANT OPTION;
+ GRANT ALL PRIVILEGES ON `computer-database-db`.* TO 'travis'@'%' WITH GRANT OPTION;
 
 
  FLUSH PRIVILEGES;
