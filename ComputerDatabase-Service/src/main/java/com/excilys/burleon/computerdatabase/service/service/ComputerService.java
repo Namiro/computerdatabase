fef2ef557a75994c0414fbf33f2222fecfecdad0
corentinarnaud <corentin.arnaud9@gmail.com>
2017-05-12 15:31:34 +0200

    fix 1 : validation

--- ComputerDatabase-Service/src/main/java/com/excilys/burleon/computerdatabase/service/service/ComputerService.java
+++ ComputerDatabase-Service/src/main/java/com/excilys/burleon/computerdatabase/service/service/ComputerService.java
@@ -1,11 +1,14 @@
 package com.excilys.burleon.computerdatabase.service.service;
 
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.excilys.burleon.computerdatabase.core.model.Company;
 import com.excilys.burleon.computerdatabase.core.model.Computer;
+import com.excilys.burleon.computerdatabase.service.exception.entityvalidation.InvalidDateException;
 import com.excilys.burleon.computerdatabase.service.exception.entityvalidation.InvalidDateOrderException;
 import com.excilys.burleon.computerdatabase.service.exception.entityvalidation.TooLongComputerNameException;
 import com.excilys.burleon.computerdatabase.service.exception.entityvalidation.TooShortComputerNameException;
@@ -34,12 +37,24 @@ public class ComputerService extends AModelService<Computer> implements ICompute
         if (entity.getName().length() < 1) {
             throw new TooShortComputerNameException("The name of computer must be longer (More then 1 caracter)");
         }
-        if (entity.getName().length() > 550) {
-            throw new TooLongComputerNameException("The name of computer must be shorter (Less then 50 caracter)");
+        if (entity.getName().length() > 250) {
+            throw new TooLongComputerNameException("The name of computer must be shorter (Less then 250 caracter)");
         }
         if (entity.getDiscontinued() != null && entity.getIntroduced() != null
                 && entity.getDiscontinued().isBefore(entity.getIntroduced())) {
             throw new InvalidDateOrderException("The discontinued date must be after the introduced date");
+        }
+        if (entity.getIntroduced() != null && entity.getIntroduced().isBefore(LocalDateTime.of(1970, 01, 01, 0, 0, 0))) {
+            throw new InvalidDateException("The introduced date must be after 1970-01-01");
+        }
+        if (entity.getDiscontinued() != null && entity.getDiscontinued().isBefore(LocalDateTime.of(1970, 01, 01, 0, 0, 0))) {
+            throw new InvalidDateException("The discontinued date must be after 1970-01-01");
+        }
+        if (entity.getIntroduced() != null && entity.getIntroduced().isAfter(LocalDateTime.of(2038, 01, 18, 0, 0, 0))) {
+            throw new InvalidDateException("The introduced date must be before 2038-01-18");
+        }
+        if (entity.getDiscontinued() != null && entity.getDiscontinued().isAfter(LocalDateTime.of(2038, 01, 18, 0, 0, 0))) {
+            throw new InvalidDateException("The discontinued date must be before 2038-01-18");
         }
         if (entity.getCompany() != null) {
             this.companyService.checkDataEntity(entity.getCompany());
